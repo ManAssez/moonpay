a41e37db28e43b1219e718468e8ca85359d2b906
root <ranto10tavela@gmail.com>
2024-01-03 11:47:54 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { MoonPayProvider } from "@moonpay/moonpay-react";
+import { MoonPayBuyWidget } from "@moonpay/moonpay-react";
+import { useState } from "react";
+function App() {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div className="App">
+      <MoonPayProvider
+        apiKey="pk_test_y59OrPlcSLmCOv43kNz2mcjgF0pf9Uw"
+        environment="sandbox"
+        debug
+      >
+        <div className="test">
+          <MoonPayBuyWidget
+            variant="embedded"
+            baseCurrencyCode="usd"
+            baseCurrencyAmount="1000"
+            defaultCurrencyCode="eth"
+            walletAddress="0xc216eD2D6c295579718dbd4a797845CdA70B3C36"
+            email="ranto10tavela@gmail.com"
+            paymentMethod="credit_debit_card"
+            language="fr"
+            showOnlyCurrencies="eth"
+            showWalletAddressForm="false"
+            visible
+          />
+        </div>
+      </MoonPayProvider>
+    </div>
+  );
+}
+
+export default App;
